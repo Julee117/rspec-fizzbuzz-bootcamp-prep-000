53d6d8b1080c0858554567852fc151594dfff426
Julianne Kim <juliannne@gmail.com>
2017-10-10 04:38:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
-  else
-    nil
   end
+
+  if num % 3 == 0
+    "Fizz"
+  end
+  
+  if num % 5 == 0
+    "Buzz"
+  end
+
 end
